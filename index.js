028761d556556cbfdaf238c74b275002576caf1a
Mukeshkumar1611 <mukesh0895.be21@chitkara.edu.in>
2024-01-18 21:20:38 +0530

    Adding all files

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => { console.log("Database is connected") })
+    .catch((err) => { console.log('could not connected to mongodb', err)})
+
+
+// MiddleWare
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get("/", (req, res) => {
+    res.send("Welcome To Homepage");
+})
+
+app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
+
+app.listen(8800, () => {
+    console.log("Backend Server is running now");
+})
