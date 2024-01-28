2a14ffa3c7e20975ff52cca425393093df397e1c
Mukeshkumar1611 <mukesh0895.be21@chitkara.edu.in>
2024-01-28 21:47:23 +0530

    Testing cors after making changes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const morgan = require("morgan");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
-// const cors = require('cors');
+const cors = require('cors');
 
 dotenv.config();
 
@@ -16,7 +16,25 @@ mongoose.connect(process.env.MONGO_URL)
     .catch((err) => { console.log('Could not connected to mongodb', err)})
 
 // MiddleWare
-// app.use(cors());
+const corsOptions = {
+    origin: 'https://lime-concerned-bull.cyclic.app/',
+    credentials: true, // Enable credentials (cookies, authorization headers, etc.)
+};
+
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', true);
+
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+    } else {
+        next();
+    }
+});
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
