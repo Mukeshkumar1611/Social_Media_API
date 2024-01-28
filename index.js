25ff5581dad82a32b2f19d199b7a4e4de2452a9e
Mukeshkumar1611 <mukesh0895.be21@chitkara.edu.in>
2024-01-28 22:44:22 +0530

    updated cors implementation

--- index.js
+++ index.js
@@ -17,23 +17,13 @@ mongoose.connect(process.env.MONGO_URL)
 
 // MiddleWare
 const corsOptions = {
-    origin: 'https://lime-concerned-bull.cyclic.app/',
+    origin: '*',
     credentials: true, // Enable credentials (cookies, authorization headers, etc.)
 };
 
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', true);
 
-    if (req.method === 'OPTIONS') {
-        res.status(200).end();
-    } else {
-        next();
-    }
-});
+
 
 app.use(cors(corsOptions));
 app.use(express.json());
