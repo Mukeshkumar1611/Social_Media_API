756ccfcec218ba92ace8efa3cde532daecbb8992
Mukeshkumar1611 <mukesh0895.be21@chitkara.edu.in>
2024-09-20 22:41:05 +0530

    Fixed Db connection

--- config/dbconnection.js
+++ config/dbconnection.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     host: process.env.DB_HOST,
     dialect: 'postgres',
     logging: false, // Set to true if you want to see SQL queries
